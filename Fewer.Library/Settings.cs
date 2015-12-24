6228459b45e9c46eeebf51f8d8718353cb1d2ca0
fil_dv <fil_dv1@bigmir.net>
2015-12-24 20:39:11 +0200

    realization service methods

--- Fewer.Library/Settings.cs
+++ Fewer.Library/Settings.cs
@@ -1,11 +1,23 @@
 using System;
+using System.Collections.Generic;
 
 namespace Fewer.Library
 {
     public class Settings
     {
-        public uint MinSize { get; set; }
-        public DateTime MinDate { get; set; }
-        public List<string> Disks { get; set; }
+        long _minSize;
+        public long MinSize { get { return _minSize; } set{ _minSize = value; } }
+        DateTime _minDate;
+        public DateTime MinDate { get { return _minDate; } set { _minDate = value; } }
+        List<string> _disk;
+        public List<string> Disks { get { return _disk; } set { _disk = value; } }
+
+        public void SetSettings(long minSize, DateTime minDate, List<string> disks)
+        {
+            _minSize = minSize;
+            _minDate = minDate;
+            _disk = disks;
+        }
+      
     }
 }
